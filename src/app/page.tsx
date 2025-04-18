4d653681e70f030798204efe10d7948c4c405da3
monirshadkani <152501836+monirshadkani@users.noreply.github.com>
2025-04-18 19:51:02 +0200

    started to create an API call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
 "use client";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import axios from 'axios'
 
 export default function Home() {
   const [taskTitle, setTaskTitle] = useState<string>("");
 
+const URL = "dkjs"
+  const createTask = async ( taskTitle: string) => {
+ try{
+ const data =   await axios.post(URL, {taskTitle})
+
+ return data
+ } catch ( error){
+  console.log(error)
+ }
+  
+
+
+  }
+
   const handleTaskSubmit = (e) => {
     e.preventDefault();
+    try {
+      setTaskTitle
+    }
   };
 
   return (
